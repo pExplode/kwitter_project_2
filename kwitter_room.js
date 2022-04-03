6ea0a13ca8f06ffd0bb9d3fcfdaabce971d398d0
pExplode <92288677+pExplode@users.noreply.github.com>
2022-04-03 10:40:06 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCFggUTg3pi_doBCQBgJT22G8X3uQWzyXI",
+    authDomain: "kwitter-project-a125d.firebaseapp.com",
+    databaseURL: "https://kwitter-project-a125d-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-a125d",
+    storageBucket: "kwitter-project-a125d.appspot.com",
+    messagingSenderId: "519775986469",
+    appId: "1:519775986469:web:be15d00d44cbf7c89030cc",
+    measurementId: "G-8QDPKZX7Z3"
+  };
+
+  
+  
